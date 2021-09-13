42ad08652eec48a4907fbfc9d77172ae931ed843
Emre YÜCE <emreyuce90@hotmail.com>
2021-09-13 15:57:17 +0300

    IoC Container added

---
+++ UdemyBlogProject.BusinessLayer/Containers/MicrosoftIOC/CustomExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UdemyBlogProject.BusinessLayer.Concrete;
+using UdemyBlogProject.BusinessLayer.Interfaces;
+using UdemyBlogProject.DataAccessLayer.Concrete.EntityFrameworkCore.Repositories;
+using UdemyBlogProject.DataAccessLayer.Interfaces;
+
+namespace UdemyBlogProject.BusinessLayer.Containers.MicrosoftIOC
+{
+    public static class CustomExtensions
+    {
+        public static void  AddDependencies(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IGenericDal<>), typeof(EfGenericRepository<>));
+            services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
+        }
+    }
+}
